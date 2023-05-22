817865a130ba382dc64cfcc7ae05e2ad7b5efd96
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-05-22 14:16:38 +0300

    Update 02-lightbox.js
    added codes

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+    gallery: document.querySelector(".gallery"),
+  };
+  
+  const markup = galleryItems
+    .map((item) => {
+      return `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+        </a></li>`;
+    }).join("");
+    
+  refs.gallery.innerHTML = markup;
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
