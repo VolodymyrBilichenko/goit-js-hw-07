cfe5bb18a29b790a0c3960c5b9ce431c541ffdb8
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2023-05-22 15:39:06 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,38 +3,38 @@ import { galleryItems } from './gallery-items.js';
 
 const refs = {
     gallery: document.querySelector(".gallery"),
-  };
+};
   
-  const markup = galleryItems
-    .map((item) => {
-      return `<li class="gallery__item">
-        <a class="gallery__link" href="${item.original}">
-        <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-source="${item.original}"/>
-        </a></li>`;
-    }).join("");
+const markup = galleryItems.map((item) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-source="${item.original}"/>
+    </a></li>`;
+}).join("");
 
-  refs.gallery.innerHTML = markup;
+refs.gallery.innerHTML = markup;
   
-  refs.gallery.addEventListener("click", openModalWindow);
-  function openModalWindow(event) {
+refs.gallery.addEventListener("click", openModalWindow);
+  
+function openModalWindow(event) {
     event.preventDefault();
     if (event.target.nodeName !== "IMG") {
-      return;
+    return;
     }
     const imageUrl = event.target.dataset.source;
     console.log(imageUrl);
     const instance = basicLightbox.create(`
       <img src="${imageUrl}" width="800" height="600">
-  `);
+    `);
     instance.show();
-  
+
     const visible = instance.visible();
-  
+
     document.addEventListener("keydown", closeModalOnEscape);
     function closeModalOnEscape(event) {
-      if (event.code === "Escape" && visible) {
+        if (event.code === "Escape" && visible) {
         instance.close();
         document.removeEventListener("keydown", closeModalOnEscape);
-      }
+        }
     }
-  }
+}
